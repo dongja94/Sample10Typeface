86b7fdb0dbb5a94581063039c2230351d47b93cd
dongja94 <dongja94@gmail.com>
2016-01-29 14:33:23 +0900

    Custom Font

--- app/src/main/java/com/example/dongja94/sampletypeface/MainActivity.java
+++ app/src/main/java/com/example/dongja94/sampletypeface/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.dongja94.sampletypeface;
 
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,8 +18,15 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    AppCompatDelegate mDelegate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (Build.VERSION.SDK_INT < 11) {
+            getLayoutInflater().setFactory(this);
+        } else {
+            getLayoutInflater().setFactory2(this);
+        }
+        mDelegate = getDelegate();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -28,14 +41,36 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        TextView textView = (TextView)findViewById(R.id.text_namun);
-        textView.setTypeface(FontManager.getInstance().getTypeface(this, FontManager.FONT_NAME_NANUM));
+//        TextView textView = (TextView)findViewById(R.id.text_namun);
+//        textView.setTypeface(FontManager.getInstance().getTypeface(this, FontManager.FONT_NAME_NANUM));
+//
+//        textView = (TextView)findViewById(R.id.text_noto);
+//        textView.setTypeface(FontManager.getInstance().getTypeface(this, FontManager.FONT_NAME_NOTO));
+//
+//        textView = (TextView)findViewById(R.id.text_roboto);
+//        textView.setTypeface(FontManager.getInstance().getTypeface(this, FontManager.FONT_NAME_ROBOTO));
+    }
 
-        textView = (TextView)findViewById(R.id.text_noto);
-        textView.setTypeface(FontManager.getInstance().getTypeface(this, FontManager.FONT_NAME_NOTO));
+    int[] ids = {android.R.attr.fontFamily};
 
-        textView = (TextView)findViewById(R.id.text_roboto);
-        textView.setTypeface(FontManager.getInstance().getTypeface(this, FontManager.FONT_NAME_ROBOTO));
+    @Override
+    public View onCreateView(String name, Context context, AttributeSet attrs) {
+        View view = super.onCreateView(name, context, attrs);
+        if (view == null) {
+            view = mDelegate.createView(null, name, context, attrs);
+        }
+        if (view == null && name.equals("TextView")) {
+            view = new TextView(context, attrs);
+        }
+        if (view != null && view instanceof TextView) {
+            TypedArray ta = context.obtainStyledAttributes(attrs, ids);
+            String fontName = ta.getString(0);
+            Typeface typeface = FontManager.getInstance().getTypeface(context, fontName);
+            if (typeface != null) {
+                ((TextView) view).setTypeface(typeface);
+            }
+        }
+        return view;
     }
 
     @Override
